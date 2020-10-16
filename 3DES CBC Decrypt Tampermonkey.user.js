50e710d0e2f681a5ab7ae477f1ac01223ae2bd47
BastianAyala <64991984+BastianAyala@users.noreply.github.com>
2020-10-15 21:18:01 -0300

    Update 3DES CBC Decrypt Tampermonkey.user.js

--- 3DES CBC Decrypt Tampermonkey.user.js
+++ 3DES CBC Decrypt Tampermonkey.user.js
@@ -25,20 +25,7 @@
     }
     
     var text = "3Hsku/b7T3AD+3XfVU1etw==";
-    /*
-    var ivHex = CryptoJS.enc.Hex.parse(CryptoJS.enc.Utf8.parse(iv).toString(CryptoJS.enc.Hex));
-
-    const str = CryptoJS.enc.Utf8.parse(text);
-    const baseStr = CryptoJS.enc.Base64.stringify(str);
-    const baseStrUtf = CryptoJS.enc.Utf8.parse(baseStr);
-    const KeyHex = CryptoJS.enc.Utf8.parse(key);
-
-    var Dencrypted = CryptoJS.TripleDES.decrypt(baseStrUtf, KeyHex, {
-	iv:ivHex,
-	mode: CryptoJS.mode.CBC,
-	padding: CryptoJS.pad.Pkcs7}
-);
-*/
+    
     function obtenerDiv(){
         return document.getElementsByTagName("div");}
 
@@ -50,8 +37,8 @@
     //console.log(ide);
 
     var r = decryptByDES(text,key, iv);
-    console.log(r);
-    r = r.toString(CryptoJS.enc.Utf8)
+   
+    r = r.toString(CryptoJS.enc.Utf8);
     console.log(r);
 
     //div.innerHTML = r;
