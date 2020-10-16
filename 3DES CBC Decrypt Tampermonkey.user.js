01e929f07c12308a7c2a5d64e72009d4f1be738c
BastianAyala <64991984+BastianAyala@users.noreply.github.com>
2020-10-16 10:45:58 -0300

    Update 3DES CBC Decrypt Tampermonkey.user.js

--- 3DES CBC Decrypt Tampermonkey.user.js
+++ 3DES CBC Decrypt Tampermonkey.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Desencriptar un codigo cifrado en python
 // @author       Bastán Ayala
-// @match        https://htmlpreview.github.io/?https://github.com/BastianAyala/Tarea3Criptograf-a-/blob/main/encode.html
+// @match        https://bastianayala.github.io/Tarea3Cripto.github.io/
 // @grant        none
 //updateURL
 // @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js
@@ -13,34 +13,22 @@
 
 (function decryp() {
     'use strict';
-    
+
     var key = "llavedesllavedesllavedes";//24
     var iv ="12345678";//8
 
     function decryptByDES(ciphertext, key, iv) {
         var keyHex = CryptoJS.enc.Utf8.parse(key);
         var ivHex = CryptoJS.enc.Hex.parse(CryptoJS.enc.Utf8.parse(iv).toString(CryptoJS.enc.Hex));
-        var decrypted = CryptoJS.DES.decrypt(ciphertext, keyHex, { iv: ivHex, mode: CryptoJS.mode.CBC });
+        var decrypted = CryptoJS.DES.decrypt(ciphertext, keyHex, { iv: ivHex, mode: CryptoJS.mode.CBC, padding:CryptoJS.pad.Pkcs7 });
         return decrypted;
     }
     
-    var text = "3Hsku/b7T3AD+3XfVU1etw==";
-    
-    function obtenerDiv(){
-        return document.getElementsByTagName("div");}
+    var div = document.getElementsByTagName("div");
+    var idTextoCifrado = div[0].id;
 
-    setTimeout(obtenerDiv,1000);
-    var div = obtenerDiv();
-    console.log(div);
+    var result = decryptByDES(idTextoCifrado,key, iv);
+    result = result.toString(CryptoJS.enc.Utf8)
 
-    //const ide = div[0].id;
-    //console.log(ide);
-
-    var r = decryptByDES(text,key, iv);
-   
-    r = r.toString(CryptoJS.enc.Utf8);
-    console.log(r);
-
-    //div.innerHTML = r;
-
+    div[0].innerHTML = 'mensaje cifrado: '+result;
 })();
